cbbeeedf5c8b6f47c9a8a2177bd610b9d305eb37
Yisroel Weisberg <weisbeym@mail.uc.edu>
2018-05-29 12:23:53 -0400

    basic setup done

---
+++ index.js
@@ -0,0 +1,19 @@
+// TODO: Get a reference to our form
+
+// TODO: Get a reference to our input
+
+function generateRandomNumber() {
+    return;
+}
+
+// TODO: Display inital random number
+
+const initialNumber = generateRandomNumber();
+
+document.getElementById("initialNumberHolder").innerHTML = `Starting number: ${ initialNumber }`;
+
+// TODO: Add "submit" event listener
+
+// TODO: Process input, generate new random number, compare
+
+// TODO: Provide user restart method
